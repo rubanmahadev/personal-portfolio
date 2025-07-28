e3a5809534000c437cc5da7c4eaf846274477274
rubanmahadev <rubanmahadev5@outlook.com>
2025-07-28 18:03:20 +0530

    - Redirect Linkes Updated - Form Submission Function and Mail Added

--- dist/script.js
+++ dist/script.js
@@ -2,3 +2,5 @@
     const nav = document.getElementById('mobile-nav');
     nav.classList.toggle('sideBarShow');
   }
+
+  document.querySelector('input[name="time"]').value = new Date().toISOString();
